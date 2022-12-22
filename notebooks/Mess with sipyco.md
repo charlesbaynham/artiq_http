e5a9b6d14e8fbff2ab2eaa9b5cd35ff31a128f5b
Charles Baynham <c.baynham@imperial.ac.uk>
2022-12-22 10:39:12 +0000

    Dict access working

--- notebooks/Mess with sipyco.md
+++ notebooks/Mess with sipyco.md
@@ -20,4 +20,65 @@ The goal is to serve an RESTful API for ARTIQ. To do this, I'll set up a FastAPI
 
 ```python
 
+from sipyco.sync_struct import Subscriber
+from sipyco.pc_rpc import AsyncioClient
+import asyncio
+
+SERVER = "labserver"
+
+async def get_dict(host, notifier_name, port=3250, timeout=3.0):
+    d = dict()
+    update_occured = asyncio.Event()
+
+    def init_d(x):
+        d.clear()
+        d.update(x)
+        return d
+    
+    def register_update(mod):
+        update_occured.set()
+
+    subscriber = Subscriber(notifier_name, target_builder=init_d,
+                            notify_cb=register_update,
+                           disconnect_cb=None)
+    
+    asyncio.create_task(subscriber.connect(host, port))
+    
+    try:
+        await asyncio.wait_for(update_occured.wait(), timeout=timeout)
+    finally:
+        await subscriber.close()
+    
+    return d
+
+```
+
+
+```python
+await get_dict(SERVER, "schedule")
+```
+
+```python
+await get_dict(SERVER, "devices")
+```
+
+```python
+await get_dict(SERVER, "datasets")
+```
+
+```python
+def submit_experiment():
+    remote = Client(args.server, port, target_name)
+        try:
+            globals()["_action_" + action](remote, args)
+        finally:
+            remote.close_rpc()
+```
+
+```python
+remote = Client(args.server, port, target_name)
+        try:
+            globals()["_action_" + action](remote, args)
+        finally:
+            remote.close_rpc()
 ```
--- notebooks/Mess with sipyco.py
+++ notebooks/Mess with sipyco.py
@@ -20,3 +20,62 @@
 # The goal is to serve an RESTful API for ARTIQ. To do this, I'll set up a FastAPI server which runs as a controller and which interfaces to artiq_master through sipyco. First, though, I'll mess around a bit here until I have a plan. 
 
 # %%
+
+from sipyco.sync_struct import Subscriber
+from sipyco.pc_rpc import AsyncioClient
+import asyncio
+
+SERVER = "labserver"
+
+async def get_dict(host, notifier_name, port=3250, timeout=3.0):
+    d = dict()
+    update_occured = asyncio.Event()
+
+    def init_d(x):
+        d.clear()
+        d.update(x)
+        return d
+    
+    def register_update(mod):
+        update_occured.set()
+
+    subscriber = Subscriber(notifier_name, target_builder=init_d,
+                            notify_cb=register_update,
+                           disconnect_cb=None)
+    
+    asyncio.create_task(subscriber.connect(host, port))
+    
+    try:
+        await asyncio.wait_for(update_occured.wait(), timeout=timeout)
+    finally:
+        await subscriber.close()
+    
+    return d
+
+
+
+# %%
+await get_dict(SERVER, "schedule")
+
+# %%
+await get_dict(SERVER, "devices")
+
+# %%
+await get_dict(SERVER, "datasets")
+
+
+# %%
+def submit_experiment():
+    remote = Client(args.server, port, target_name)
+        try:
+            globals()["_action_" + action](remote, args)
+        finally:
+            remote.close_rpc()
+
+
+# %%
+remote = Client(args.server, port, target_name)
+        try:
+            globals()["_action_" + action](remote, args)
+        finally:
+            remote.close_rpc()
