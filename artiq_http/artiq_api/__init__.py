f94ea4563ddbac1f971909db0ce2476ac1336443
Charles Baynham <c.baynham@imperial.ac.uk>
2022-12-22 13:16:54 +0000

    Reinstate cancel_experiment

---
+++ artiq_http/artiq_api/__init__.py
@@ -0,0 +1,3 @@
+from . import control_schedule
+from . import models
+from . import notifiers
--- artiq_http/main.py
+++ artiq_http/main.py
@@ -2,9 +2,9 @@ from typing import Dict
 
 from fastapi import FastAPI
 
+from . import artiq_api as api
 from . import patch_pydantic_numpy
-from .artiq_api import notifiers
-from .artiq_api.models import ScheduleItem
+
 
 app = FastAPI()
 
@@ -15,8 +15,8 @@ async def root():
 
 
 @app.get("/schedule")
-async def get_schedule() -> Dict[int, ScheduleItem]:
-    return await notifiers.get_schedule()
+async def get_schedule() -> Dict[int, api.models.ScheduleItem]:
+    return await api.notifiers.get_schedule()
 
 
 @app.get("/devices")
@@ -26,7 +26,7 @@ async def get_devices() -> dict:
     Returns:
         dict: ARTIQ Device_DB
     """
-    return await notifiers.get_devices()
+    return await api.notifiers.get_devices()
 
 
 @app.get("/datasets")
@@ -38,17 +38,17 @@ async def get_datasets() -> dict:
     Returns:
         dict: All broadcasted ARTIQ datasets
     """
-    return await notifiers.get_datasets()
+    return await api.notifiers.get_datasets()
 
 
-# @app.post("/cancel")
-# async def cancel_experiment(rid:int, force:bool=False) -> None:
-#     """Cancel a running experiment
+@app.post("/cancel")
+async def cancel_experiment(rid: int, force: bool = False) -> None:
+    """Cancel a running experiment
 
-#     Args:
-#         rid (int): RID of the experiment to cancel
-#         force (bool): If True, forcibly close the experiment instead of requesting closure
-#     """
-#     datasets = await
+    Args:
+        rid (int): RID of the experiment to cancel
+        force (bool): If True, forcibly close the experiment instead of requesting closure
+    """
+    # datasets = await
 
-#     return control_schedule.cancel_experiment(SERVER, rid, force)
+    return api.control_schedule.cancel_experiment(rid, force)
