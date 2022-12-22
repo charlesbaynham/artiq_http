19bb9ea4e3d2c0ccfef414471bfc2e256cb50f17
Charles Baynham <c.baynham@imperial.ac.uk>
2022-12-22 10:56:32 +0000

    Done with notebooks

--- notebooks/Mess with sipyco.md
+++ notebooks/Mess with sipyco.md
@@ -16,15 +16,18 @@ jupyter:
 ARTIQ via HTTP
 =====
 
-The goal is to serve an RESTful API for ARTIQ. To do this, I'll set up a FastAPI server which runs as a controller and which interfaces to artiq_master through sipyco. First, though, I'll mess around a bit here until I have a plan. 
+The goal is to serve an RESTful API for ARTIQ. To do this, I'll set up a FastAPI server which runs as a controller and which interfaces to artiq_master through sipyco. First, though, I'll mess around a bit here until I have a plan.
 
 ```python
-
 from sipyco.sync_struct import Subscriber
-from sipyco.pc_rpc import AsyncioClient
+from sipyco.pc_rpc import Client
 import asyncio
+import logging
+import time
+from dateutil.parser import parse as parse_date
 
 SERVER = "labserver"
+
 
 async def get_dict(host, notifier_name, port=3250, timeout=3.0):
     d = dict()
@@ -34,23 +37,22 @@ async def get_dict(host, notifier_name, port=3250, timeout=3.0):
         d.clear()
         d.update(x)
         return d
-    
+
     def register_update(mod):
         update_occured.set()
 
     subscriber = Subscriber(notifier_name, target_builder=init_d,
                             notify_cb=register_update,
-                           disconnect_cb=None)
-    
+                            disconnect_cb=None)
+
     asyncio.create_task(subscriber.connect(host, port))
-    
+
     try:
         await asyncio.wait_for(update_occured.wait(), timeout=timeout)
     finally:
         await subscriber.close()
-    
-    return d
 
+    return d
 ```
 
 
@@ -66,19 +68,58 @@ await get_dict(SERVER, "devices")
 await get_dict(SERVER, "datasets")
 ```
 
+
 ```python
-def submit_experiment():
-    remote = Client(args.server, port, target_name)
-        try:
-            globals()["_action_" + action](remote, args)
-        finally:
-            remote.close_rpc()
+async def submit_experiment(server, target_name, args, port=3251):
+    remote = AsyncioClient(server, port, target_name)
+    try:
+        expid = {
+            "log_level": logging.WARNING,  # + args.quiet*10 - args.verbose*10,
+            "class_name": args.class_name,
+            "arguments": args.arguments,
+        }
+
+        expid["file"] = args.file
+        if args.repository:
+            expid["repo_rev"] = args.revision
+
+        if args.timed is None:
+            due_date = None
+        else:
+            due_date = time.mktime(parse_date(args.timed).timetuple())
+        rid = remote.submit(args.pipeline, expid,
+                            args.priority, due_date, args.flush)
+
+        return rid
+    finally:
+        remote.close_rpc()
 ```
 
 ```python
-remote = Client(args.server, port, target_name)
-        try:
-            globals()["_action_" + action](remote, args)
-        finally:
-            remote.close_rpc()
+async def cancel_experiment(server, rid, force=False, port=3251):
+    remote = Client(server, port, "master_schedule")
+    try:
+        if not force:
+            remote.request_termination(rid)
+        else:
+            remote.delete(rid)
+    finally:
+        remote.close_rpc()
+```
+
+
+```python
+await get_dict(SERVER, "schedule")
+```
+
+```python
+await cancel_experiment(SERVER, 913)
+```
+
+```python
+await get_dict(SERVER, "schedule")
+```
+
+```python
+
 ```
--- notebooks/Mess with sipyco.py
+++ notebooks/Mess with sipyco.py
@@ -12,20 +12,22 @@
 #     language: python
 #     name: python3
 # ---
-
 # %% [markdown]
 # ARTIQ via HTTP
 # =====
 #
-# The goal is to serve an RESTful API for ARTIQ. To do this, I'll set up a FastAPI server which runs as a controller and which interfaces to artiq_master through sipyco. First, though, I'll mess around a bit here until I have a plan. 
-
+# The goal is to serve an RESTful API for ARTIQ. To do this, I'll set up a FastAPI server which runs as a controller and which interfaces to artiq_master through sipyco. First, though, I'll mess around a bit here until I have a plan.
 # %%
-
-from sipyco.sync_struct import Subscriber
-from sipyco.pc_rpc import AsyncioClient
 import asyncio
+import logging
+import time
+
+from dateutil.parser import parse as parse_date
+from sipyco.pc_rpc import Client
+from sipyco.sync_struct import Subscriber
 
 SERVER = "labserver"
+
 
 async def get_dict(host, notifier_name, port=3250, timeout=3.0):
     d = dict()
@@ -35,23 +37,25 @@ async def get_dict(host, notifier_name, port=3250, timeout=3.0):
         d.clear()
         d.update(x)
         return d
-    
+
     def register_update(mod):
         update_occured.set()
 
-    subscriber = Subscriber(notifier_name, target_builder=init_d,
-                            notify_cb=register_update,
-                           disconnect_cb=None)
-    
+    subscriber = Subscriber(
+        notifier_name,
+        target_builder=init_d,
+        notify_cb=register_update,
+        disconnect_cb=None,
+    )
+
     asyncio.create_task(subscriber.connect(host, port))
-    
+
     try:
         await asyncio.wait_for(update_occured.wait(), timeout=timeout)
     finally:
         await subscriber.close()
-    
-    return d
 
+    return d
 
 
 # %%
@@ -65,17 +69,51 @@ await get_dict(SERVER, "datasets")
 
 
 # %%
-def submit_experiment():
-    remote = Client(args.server, port, target_name)
-        try:
-            globals()["_action_" + action](remote, args)
-        finally:
-            remote.close_rpc()
+async def submit_experiment(server, target_name, args, port=3251):
+    remote = AsyncioClient(server, port, target_name)
+    try:
+        expid = {
+            "log_level": logging.WARNING,  # + args.quiet*10 - args.verbose*10,
+            "class_name": args.class_name,
+            "arguments": args.arguments,
+        }
+
+        expid["file"] = args.file
+        if args.repository:
+            expid["repo_rev"] = args.revision
+
+        if args.timed is None:
+            due_date = None
+        else:
+            due_date = time.mktime(parse_date(args.timed).timetuple())
+        rid = remote.submit(args.pipeline, expid, args.priority, due_date, args.flush)
+
+        return rid
+    finally:
+        remote.close_rpc()
 
 
 # %%
-remote = Client(args.server, port, target_name)
-        try:
-            globals()["_action_" + action](remote, args)
-        finally:
-            remote.close_rpc()
+
+
+async def cancel_experiment(server, rid, force=False, port=3251):
+    remote = Client(server, port, "master_schedule")
+    try:
+        if not force:
+            remote.request_termination(rid)
+        else:
+            remote.delete(rid)
+    finally:
+        remote.close_rpc()
+
+
+# %%
+await get_dict(SERVER, "schedule")
+
+# %%
+await cancel_experiment(SERVER, 913)
+
+# %%
+await get_dict(SERVER, "schedule")
+
+# %%
