50a59774cb26769d5e1d07ccb26d5d5377203de9
Charles Baynham <c.baynham@imperial.ac.uk>
2022-12-22 11:59:03 +0000

    Adding more

--- artiq_http/main.py
+++ artiq_http/main.py
@@ -13,3 +13,25 @@ async def root():
 @app.get("/schedule")
 async def get_schedule():
     return await get_dict(SERVER, "schedule")
+
+
+@app.get("/devices")
+async def get_devices():
+    """Get the current device_db
+
+    Returns:
+        dict: ARTIQ Device_DB
+    """
+    return await get_dict(SERVER, "devices")
+
+
+@app.get("/datasets")
+async def get_datasets() -> dict:
+    """Get all existing ARTIQ datasets
+
+    This method might return a large output
+
+    Returns:
+        dict: All broadcasted ARTIQ datasets
+    """
+    return await get_dict(SERVER, "datasets")
---
+++ tests/test_dicts.py
@@ -0,0 +1,26 @@
+from fastapi.testclient import TestClient
+
+from artiq_http.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+
+def test_schedule():
+    response = client.get("/schedule")
+    assert response.status_code == 200
+
+
+def test_devices():
+    response = client.get("/devices")
+    assert response.status_code == 200
+
+
+def test_datasets():
+    response = client.get("/datasets")
+    assert response.status_code == 200
