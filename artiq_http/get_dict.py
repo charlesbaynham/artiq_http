c21935d31cb3a6f49d8f2b689a79dc8ac3b32836
Charles Baynham <c.baynham@imperial.ac.uk>
2022-12-22 11:46:17 +0000

    Add schedule getter

---
+++ artiq_http/get_dict.py
@@ -0,0 +1,54 @@
+
+
+import asyncio
+import logging
+
+
+from sipyco.sync_struct import Subscriber
+
+logger = logging.getLogger(__name__)
+
+
+async def get_dict(host, notifier_name, port=3250, timeout=3.0):
+    """
+    Get a dict that's being served from a Sipyco sync_strut :class:`.Notifier`
+
+    Args:
+        host (str): Server to connect to
+        notifier_name (str): Name of RPC notifier
+        port (int, optional): Network port. Defaults to 3250.
+        timeout (float, optional): Timeout before giving up. Defaults to 3.0.
+
+    Returns:
+        dict: The dict returned from the server
+    """
+    d = dict()
+    update_occured = asyncio.Event()
+
+    logger.debug("Getting dict from notifier %s", notifier_name)
+
+    def init_d(x):
+        d.clear()
+        d.update(x)
+        return d
+
+    def register_update(_):
+        update_occured.set()
+
+    subscriber = Subscriber(
+        notifier_name,
+        target_builder=init_d,
+        notify_cb=register_update,
+        disconnect_cb=None,
+    )
+
+    asyncio.create_task(subscriber.connect(host, port))
+
+    try:
+        logger.debug("Waiting for successful update")
+        await asyncio.wait_for(update_occured.wait(), timeout=timeout)
+        logger.debug("Update received: %s", d)
+    finally:
+        await subscriber.close()
+
+    return d
--- artiq_http/main.py
+++ artiq_http/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
-
+from .get_dict import get_dict
 app = FastAPI()
+
+SERVER = "labserver"
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/schedule")
+async def get_schedule():
+    return await get_dict(SERVER, "schedule")
