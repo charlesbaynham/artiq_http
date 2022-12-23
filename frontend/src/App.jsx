94613b2f7f1bd08390866fd543fca35f637332d4
Charles Baynham <c.baynham@imperial.ac.uk>
2022-12-23 11:29:52 +0000

    Add component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
+import Schedule from "./schedule";
+
 export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
+  <Schedule />
 );
---
+++ frontend/src/schedule.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Schedule() {
+    return (
+        <>
+            <p>This is the schedule of what's up</p>
+        </>
+    )
+}
+
+export default Schedule;
