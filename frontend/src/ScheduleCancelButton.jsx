85728f5b00fd285ff0869f4b0efcee6371ebe0a8
Charles Baynham <c.baynham@imperial.ac.uk>
2022-12-23 13:47:07 +0000

    Wire button

--- frontend/src/ScheduleCancelButton.jsx
+++ frontend/src/ScheduleCancelButton.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 import Button from 'react-bootstrap/Button';
 
-function ScheduleCancelButton() {
-    return <Button className="mt-3" variant="danger">Request Termination</Button>
+function ScheduleCancelButton(props) {
+    const [isLoading, setLoading] = React.useState(false);
+
+    const handleClick = () => setLoading(true)
+
+    React.useEffect(() => {
+        if (isLoading) {
+            console.log(`RID ${props.rid} cancelled`)
+            setLoading(false)
+        }
+    }, [isLoading])
+
+    return <Button
+        className="mt-3"
+        variant="danger"
+        disabled={isLoading}
+        onClick={!isLoading ? handleClick : null}
+    >Request Termination</Button>
 }
 
 export default ScheduleCancelButton;
