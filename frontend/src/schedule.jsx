52d3eb25b7ce18e0e1599636fe2c11bdb85424b4
Charles Baynham <c.baynham@imperial.ac.uk>
2022-12-23 12:08:47 +0000

    Kinda something...

--- frontend/src/schedule.jsx
+++ frontend/src/schedule.jsx
@@ -2,27 +2,52 @@ import React from 'react';
 
 
 function Schedule() {
-    const [posts, setPosts] = React.useState([]);
+    const [exps, setExps] = React.useState({});
 
     React.useEffect(() => {
-        fetch('http://localhost:8000')
+        fetch('http://localhost:8000/schedule')
             .then((response) => response.json())
             .then((data) => {
+                console.log("Received:")
                 console.log(data);
-                setPosts(data);
+                setExps(data);
             })
             .catch((err) => {
                 console.log(err.message);
             });
     }, []);
 
-    let buildList = () => {
-        return <>
-            {
-                posts.map((post, ind) => { return <li key={ind}>{post.title}</li> }
-                )
-            }
-        </>
+    const objectMap = (obj, fn) =>
+        Object.fromEntries(
+            Object.entries(obj).map(
+                ([k, v], i) => [k, fn(v, k, i)]
+            )
+        )
+
+    const buildList = () => {
+        // return <p>Placeholder</p>
+
+        const rids = Object.keys(exps)
+        const list2 = Object.keys(exps).map((rid) => {
+            <li>{rid}</li>
+        })
+
+        const list = objectMap(exps, (rid, exp, ind) => {
+            return <li>{rid}</li>
+        })
+        console.log("Exps = ")
+        console.log(exps)
+        console.log("list2=")
+        console.log(list2)
+        console.log(rids)
+
+        const mapped = rids.map((k, i) => { <p>{i}</p> })
+
+        console.log("mapped")
+        console.log(mapped)
+
+
+        return <>{rids}</>
     }
 
     return (
