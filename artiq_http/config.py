f5b7be9002a495fe17db13fcafc4f9be3c6b3b86
Charles Baynham <c.baynham@imperial.ac.uk>
2022-12-24 11:13:08 +0000

    Stop hardcoding "labserver"

--- artiq_http/config.py
+++ artiq_http/config.py
@@ -1,7 +1,7 @@
 import os
 
 config = {
-    "host": "labserver",
+    "host": "::1",
     "port_notifiers": 3250,
     "port_clients": 3251,
     "dev_mode": "NODE_ENV" in os.environ and os.environ["NODE_ENV"] == "development",
