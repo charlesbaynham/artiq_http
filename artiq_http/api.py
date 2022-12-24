0ba20e92fedc7c3c50e6258a360b6df2c8534572
Charles Baynham <c.baynham@imperial.ac.uk>
2022-12-24 16:23:44 +0000

    Also run an RPC server

---
+++ artiq_http/api.py
@@ -0,0 +1,121 @@
+from pathlib import Path
+from typing import Dict
+
+from fastapi import APIRouter
+from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+from . import artiq_api as api
+from . import patch_pydantic_numpy
+from .config import config
+
+app = FastAPI()
+
+router = APIRouter()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5172",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@router.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@router.get("/schedule")
+async def get_schedule() -> Dict[int, api.models.ScheduleItem]:
+    return await api.notifiers.get_schedule()
+
+
+@router.get("/devices")
+async def get_devices() -> dict:
+    """Get the current device_db
+
+    Returns:
+        dict: ARTIQ Device_DB
+    """
+    return await api.notifiers.get_devices()
+
+
+@router.get("/datasets")
+async def get_datasets() -> dict:
+    """Get all existing ARTIQ datasets
+
+    This method might return a large output
+
+    Returns:
+        dict: All broadcasted ARTIQ datasets
+    """
+    return await api.notifiers.get_datasets()
+
+
+@router.post("/cancel")
+async def cancel_experiment(rid: int, force: bool = False) -> None:
+    """Cancel a running experiment
+
+    Args:
+        rid (int): RID of the experiment to cancel
+        force (bool): If True, forcibly close the experiment instead of requesting closure
+    """
+    schedule = await api.notifiers.get_schedule()
+
+    if rid not in schedule:
+        raise HTTPException(404, f"RID {rid} not found in schedule")
+
+    return await api.control_schedule.cancel_experiment(rid, force)
+
+
+@router.get("/explist")
+async def get_explist() -> api.models.ExperimentList:
+    return await api.notifiers.get_explist()
+
+
+@router.post("/schedule")
+async def submit_experiment(
+    expid: api.models.ExpID,
+    pipeline: str = "main",
+    priority: int = 0,
+    flush: bool = False,
+    due_date: float = None,
+) -> None:
+    try:
+        return await api.control_schedule.submit_experiment(
+            expid,
+            pipeline,
+            priority,
+            flush,
+            due_date,
+        )
+    except ValueError as e:
+        raise HTTPException(422, str(e))
+
+
+app.include_router(router, prefix="/api")
+
+if config["dev_mode"]:
+    print("Development mode")
+
+    @app.get("/")
+    async def index():
+        return "HTML hosting is disabled in dev mode - access the react server from 'npm run frontend' directly"
+
+else:
+    print("Production mode")
+    app.mount(
+        "/",
+        StaticFiles(directory=Path(__file__, "../static").resolve(), html=True),
+        name="static",
+    )
--- artiq_http/main.py
+++ artiq_http/main.py
@@ -1,127 +1,62 @@
-from pathlib import Path
-from typing import Dict
+import argparse
+import asyncio
+import logging
+import sys
 
-from fastapi import APIRouter
-from fastapi import FastAPI
-from fastapi import HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
+import uvicorn
+from sipyco import common_args
+from sipyco.pc_rpc import Server
 
-from . import artiq_api as api
-from . import patch_pydantic_numpy
-from .config import config
+from .api import app as fastapi_app
 
-app = FastAPI()
-
-router = APIRouter()
-
-origins = [
-    "http://localhost",
-    "http://localhost:5172",
-    "http://localhost:5173",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+logger = logging.getLogger(__name__)
 
 
-@router.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@router.get("/schedule")
-async def get_schedule() -> Dict[int, api.models.ScheduleItem]:
-    return await api.notifiers.get_schedule()
-
-
-@router.get("/devices")
-async def get_devices() -> dict:
-    """Get the current device_db
-
-    Returns:
-        dict: ARTIQ Device_DB
-    """
-    return await api.notifiers.get_devices()
-
-
-@router.get("/datasets")
-async def get_datasets() -> dict:
-    """Get all existing ARTIQ datasets
-
-    This method might return a large output
-
-    Returns:
-        dict: All broadcasted ARTIQ datasets
-    """
-    return await api.notifiers.get_datasets()
-
-
-@router.post("/cancel")
-async def cancel_experiment(rid: int, force: bool = False) -> None:
-    """Cancel a running experiment
-
-    Args:
-        rid (int): RID of the experiment to cancel
-        force (bool): If True, forcibly close the experiment instead of requesting closure
-    """
-    schedule = await api.notifiers.get_schedule()
-
-    if rid not in schedule:
-        raise HTTPException(404, f"RID {rid} not found in schedule")
-
-    return await api.control_schedule.cancel_experiment(rid, force)
-
-
-@router.get("/explist")
-async def get_explist() -> api.models.ExperimentList:
-    return await api.notifiers.get_explist()
-
-
-@router.post("/schedule")
-async def submit_experiment(
-    expid: api.models.ExpID,
-    pipeline: str = "main",
-    priority: int = 0,
-    flush: bool = False,
-    due_date: float = None,
-) -> None:
-    try:
-        return await api.control_schedule.submit_experiment(
-            expid,
-            pipeline,
-            priority,
-            flush,
-            due_date,
-        )
-    except ValueError as e:
-        raise HTTPException(422, str(e))
-
-
-app.include_router(router, prefix="/api")
-
-if config["dev_mode"]:
-    print("Development mode")
-
-    @app.get("/")
-    async def index():
-        return "HTML hosting is disabled in dev mode - access the react server from 'npm run frontend' directly"
-
-else:
-    print("Production mode")
-    app.mount(
-        "/",
-        StaticFiles(directory=Path(__file__, "../static").resolve(), html=True),
-        name="static",
+def get_argparser():
+    parser = argparse.ArgumentParser(
+        description="""ARTIQ RESTful API and basic web interface."""
     )
+    parser.add_argument(
+        "--http-port",
+        default=8000,
+        help="Port for HTTP interface (as opposed to RPC server)",
+    )
+
+    common_args.simple_network_args(parser, 10001)
+    common_args.verbosity_args(parser)
+    return parser
 
 
 def main():
-    import uvicorn
+    args = get_argparser().parse_args()
+    common_args.init_logger_from_args(args)
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    class TrivialServer:
+        def ping():
+            return True
+
+    async def run_rpc_server():
+        logger.info("Starting trivial RPC server.")
+        server = Server({"trivial": TrivialServer()}, None, True)
+
+        await server.start(common_args.bind_address_from_args(args), args.port)
+        try:
+            await server.wait_terminate()
+        finally:
+            await server.stop()
+
+    def run_fastapi_server():
+        uvicorn.run(fastapi_app, host=args.host, port=args.http_port)
+
+    loop = asyncio.get_event_loop()
+
+    loop.create_task(loop.run_in_executor(None, run_fastapi_server))
+    try:
+        loop.run_until_complete(run_rpc_server())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
+
+    if __name__ == "__main__":
+        main()
