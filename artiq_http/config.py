ff70b1f2c5ad8eb7a1d1de42228321ccb92a7bf1
Charles Baynham <c.baynham@imperial.ac.uk>
2026-01-08 21:59:59 +0000

    Back to old version

--- artiq_http/config.py
+++ artiq_http/config.py
@@ -1,7 +1,7 @@
 import os
 
 config = {
-    "host": "::1",
+    "host": "10.137.1.252",
     "port_notifiers": 3250,
     "port_clients": 3251,
     "dev_mode": "NODE_ENV" in os.environ and os.environ["NODE_ENV"] == "development",
