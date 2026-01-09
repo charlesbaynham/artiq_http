2a72db202fd02b788158eb4121876825325f7d7e
Charles Baynham <c.baynham@imperial.ac.uk>
2026-01-09 15:27:38 +0000

    Support new artiq RPC naming

--- artiq_http/artiq_api/control_schedule.py
+++ artiq_http/artiq_api/control_schedule.py
@@ -3,8 +3,6 @@ from sipyco.pc_rpc import Client
 from ..config import config
 from .models import ExpID
 
-SERVER = "labserver"
-
 
 async def submit_experiment(
     expid: ExpID,
@@ -13,7 +11,11 @@ async def submit_experiment(
     flush: bool = False,
     due_date: float = None,
 ) -> int:
-    remote = Client(config["host"], config["port_clients"], "master_schedule")
+    rpc_name = "schedule"
+    if config["old_artiq_support"]:
+        rpc_name = "master_schedule"
+
+    remote = Client(config["host"], config["port_clients"], rpc_name)
     try:
         rid = remote.submit(pipeline, expid.dict(), priority, due_date, flush)
         return rid
@@ -22,7 +24,11 @@ async def submit_experiment(
 
 
 async def cancel_experiment(rid, force=False):
-    remote = Client(config["host"], config["port_clients"], "master_schedule")
+    rpc_name = "schedule"
+    if config["old_artiq_support"]:
+        rpc_name = "master_schedule"
+    remote = Client(config["host"], config["port_clients"], rpc_name)
+
     try:
         if not force:
             remote.request_termination(rid)
